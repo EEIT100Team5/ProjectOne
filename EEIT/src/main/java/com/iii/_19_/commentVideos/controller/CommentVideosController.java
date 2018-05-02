9ec4a196ab85a47d29f1bc59961a13a39dc76fe4
lien <lien@MSI>
2018-05-02 18:05:21 +0800

    backstage error update

--- EEIT/src/main/java/com/iii/_19_/commentVideos/controller/CommentVideosController.java
+++ EEIT/src/main/java/com/iii/_19_/commentVideos/controller/CommentVideosController.java
@@ -2,6 +2,8 @@ package com.iii._19_.commentVideos.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
