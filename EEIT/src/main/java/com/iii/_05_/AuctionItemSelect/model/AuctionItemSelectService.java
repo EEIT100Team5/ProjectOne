7e9345788f7d44a18b6faa5c3cb795f058d27712
張紘笙 <35601322+strike5931@users.noreply.github.com>
2018-05-03 16:32:28 +0800

    DAO 02comp/ service 01/ controllerX

--- EEIT/src/main/java/com/iii/_05_/AuctionItemSelect/model/AuctionItemSelectService.java
+++ EEIT/src/main/java/com/iii/_05_/AuctionItemSelect/model/AuctionItemSelectService.java
@@ -1,5 +1,19 @@
 package com.iii._05_.AuctionItemSelect.model;
 
-public interface AuctionItemSelectService {
+import java.util.List;
 
+public interface AuctionItemSelectService {
+public List<AuctionItemSelectBean> getAuctionByAuctionSeqNo(Integer auctionSeqNo);
+	
+	public List<AuctionItemSelectBean> getAuctionByProductSeqNo(Integer productSeqNo);
+	
+	public List<AuctionItemSelectBean> getAllLiveAuction();
+	
+	public List<AuctionItemSelectBean> getAuctionByAccount(String account);
+	
+	public int saveAuction(AuctionItemSelectBean AuctionItemSelectBean);
+
+	public void updateAuction(AuctionItemSelectBean AuctionItemSelectBean);
+
+	public void deleteAuction(AuctionItemSelectBean AuctionItemSelectBean);
 }
