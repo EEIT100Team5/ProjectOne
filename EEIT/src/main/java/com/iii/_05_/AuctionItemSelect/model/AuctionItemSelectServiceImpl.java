b86f522254eb24641108dcc1f12d8c96d13f8b87
張紘笙 <35601322+strike5931@users.noreply.github.com>
2018-05-07 17:33:50 +0800

    BID INSERT COMPLETE%%%Auction INSERT COMPLETE <3<3<3<3<3<3<3

--- EEIT/src/main/java/com/iii/_05_/AuctionItemSelect/model/AuctionItemSelectServiceImpl.java
+++ EEIT/src/main/java/com/iii/_05_/AuctionItemSelect/model/AuctionItemSelectServiceImpl.java
@@ -2,6 +2,8 @@ package com.iii._05_.AuctionItemSelect.model;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
@@ -9,42 +11,42 @@ public class AuctionItemSelectServiceImpl implements AuctionItemSelectService {
 
 	@Autowired
 	AuctionItemSelectDAO AuctionItemSelectDAO;
-	
+	@Transactional
 	@Override
 	public List<AuctionItemSelectBean> getAuctionByAuctionSeqNo(Integer auctionSeqNo) {
 		
 		return AuctionItemSelectDAO.getAuctionByAuctionSeqNo(auctionSeqNo);
 	}
-
+	@Transactional
 	@Override
 	public List<AuctionItemSelectBean> getAuctionByProductSeqNo(Integer productSeqNo) {
 		
 		return AuctionItemSelectDAO.getAuctionByProductSeqNo(productSeqNo);
 	}
-
+	@Transactional
 	@Override
 	public List<AuctionItemSelectBean> getAllLiveAuction() {
 		return AuctionItemSelectDAO.getAllLiveAuction();
 	}
-
+	@Transactional
 	@Override
 	public List<AuctionItemSelectBean> getAuctionByAccount(String account) {
 
 		return AuctionItemSelectDAO.getAuctionByAccount(account);
 	}
-
+	@Transactional
 	@Override
 	public int saveAuction(AuctionItemSelectBean AuctionItemSelectBean) {
 		
 		return AuctionItemSelectDAO.saveAuction(AuctionItemSelectBean);
 	}
-
+	@Transactional
 	@Override
 	public void updateAuction(AuctionItemSelectBean AuctionItemSelectBean) {
 		AuctionItemSelectDAO.updateAuction(AuctionItemSelectBean);
 		
 	}
-
+	@Transactional
 	@Override
 	public void deleteAuction(AuctionItemSelectBean AuctionItemSelectBean) {
 		AuctionItemSelectDAO.deleteAuction(AuctionItemSelectBean);
--- EEIT/src/main/java/com/iii/_05_/InputLiveStreamTime/controller/InputLiveStreamTimeController.java
+++ EEIT/src/main/java/com/iii/_05_/InputLiveStreamTime/controller/InputLiveStreamTimeController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.iii._05_.AuctionItemSelect.model.AuctionItemSelectBean;
+import com.iii._05_.Bid.model.BidBean;
 import com.iii._05_.InputLiveStreamTime.model.InputLiveStreamTimeBean;
 import com.iii._05_.InputLiveStreamTime.model.InputLiveStreamTimeService;
 import com.iii._19_.commentVideos.model.CommentVideosBean;
@@ -24,16 +27,28 @@ public class InputLiveStreamTimeController {
 	
 	
 	@RequestMapping("/LiveStream")
-	public String getLiveStreamsBySeqNo(@RequestParam("LiveStreamSeqNo") Integer LiveStreamSeqNo,Model model) {
+	public String getLiveStreamsBySeqNo(@RequestParam("LiveNo") Integer LiveNo,Model model) {
 		
 		
 //		List<InputLiveStreamTimeBean> InputLiveStreamTimeBeanList =  InputLiveStreamTimeService.getLiveStreamsByStreamName(streamName);
 		
-		model.addAttribute("LiveStream", InputLiveStreamTimeService.getLiveStreamsBySeqNo(LiveStreamSeqNo));
+		model.addAttribute("LiveStream", InputLiveStreamTimeService.getLiveStreamsBySeqNo(LiveNo));
 		
 		return "LiveStreamRoom/LiveStreamRoom";
 	}
+
+	@ModelAttribute("BidBean")
+	public BidBean getBidBean() {
+		System.out.println("BibBean here");
+		return new BidBean();
 	
+	}
+	@ModelAttribute("AuctionItemSelectBean")
+	public AuctionItemSelectBean getAuctionItemSelectBean() {
+		System.out.println("AuctionItemSelectBean here");
+		return new AuctionItemSelectBean();
+	
+	}
 //	@RequestMapping(method = RequestMethod.GET)
 //	public String getAllLiveStreams() {
 //		List<InputLiveStreamTimeBean> InputLiveStreamTimeBeanList = InputLiveStreamTimeService.getAllLiveStreams();
