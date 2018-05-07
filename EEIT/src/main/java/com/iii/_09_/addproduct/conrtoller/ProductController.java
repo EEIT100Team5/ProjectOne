ca7f5c01286eb5f6b57ce9f2d3240789555c831a
vic1529 <vic2012coke@gmail.com>
2018-05-07 16:17:30 +0800

    新增商品完成

--- EEIT/src/main/java/com/iii/_09_/addproduct/conrtoller/ProductController.java
+++ EEIT/src/main/java/com/iii/_09_/addproduct/conrtoller/ProductController.java
@@ -1,24 +1,22 @@
 package com.iii._09_.addproduct.conrtoller;
 
-import java.sql.Date;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
+import java.sql.Timestamp;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.iii._09_.addproduct.model.ProductBean;
 import com.iii._09_.addproduct.model.ProductService;
+
 
 @Controller
 public class ProductController {
@@ -39,23 +37,43 @@ public class ProductController {
 //		productBean bean = (productBean) session.getAttribute("");	
 //		MemberBean bean = (MemberBean) session.getAttribute("LoginOK");
 //		System.out.println(bean);	
-		System.out.println("TESSSSSSSSSSSSSSSSST");
+		System.out.println("有效才敢大聲");
 //		map.put("productBean", new productBean());
 		return "Product/InsertProductSimple";
 	}
 	
+	
 	@RequestMapping(value = "/insertProduct",method = RequestMethod.POST)
-	public String insertProduct(@ModelAttribute("productBean") ProductBean pro,HttpSession session) throws SQLException{
+	public String insertProduct(
+			@ModelAttribute("productBean") ProductBean pro,
+			HttpSession session,HttpServletRequest request) throws SQLException {
 		
+		System.out.println("有出現才有效");
+
+		Timestamp ts = new java.sql.Timestamp(System.currentTimeMillis());
+		pro.setProDate(ts);
 //		productBean bean = (productBean) session.getAttribute("");
-		
+//		 得到一個multipart文件 並取出檔名(originalFilename)
+//		 副檔名(extImage)
+		MultipartFile quesImage = pro.getProPic();
+		String originalFilename = quesImage.getOriginalFilename();
+		pro.setProPicPath(originalFilename);
+
+		// 取出影片封面圖片副檔名
+		String extImage = originalFilename.substring(originalFilename.lastIndexOf("."));
+		ProductBean pro2 = proService.saveImage(pro, extImage, quesImage);
+		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
 //		MemberBean bean = (MemberBean) session.getAttribute("LoginOK");
 //		System.out.println(bsw3;
-		proService.insert(pro);
+		try {
+			proService.insert(pro);
+			request.setAttribute("ok", pro2);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 //			request.setAttribute("insertok", mb2);
-	
-		
-		System.out.println(pro);
+		System.out.println(pro2);
 		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		return "Product/success";
 	}
