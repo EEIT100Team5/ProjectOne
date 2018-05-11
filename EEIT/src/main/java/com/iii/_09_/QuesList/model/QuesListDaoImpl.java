f799d87e7601f4350606dfea23744c00a2a21981
vic1529 <vic2012coke@gmail.com>
2018-05-11 10:00:19 +0800

    queslist上傳

--- EEIT/src/main/java/com/iii/_09_/QuesList/model/QuesListDaoImpl.java
+++ EEIT/src/main/java/com/iii/_09_/QuesList/model/QuesListDaoImpl.java
@@ -7,9 +7,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public class QuesListDaoImpl implements QuesListDao {
 	@Autowired	
 	SessionFactory factory;
--- EEIT/src/main/webapp/WEB-INF/views/_09_/QuesList/uploadQuesList.jsp
+++ EEIT/src/main/webapp/WEB-INF/views/_09_/QuesList/uploadQuesList.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h2>01</h2>
+
 </body>
 </html>
