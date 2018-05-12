d23ba37a1a97754449119bc791fb236fb922c2cb
AXBXSD <axbxsd10@gmail.com>
2018-05-12 13:29:33 +0800

    管理員登入v1

--- EEIT/src/main/java/com/iii/_01_/Manager/controller/ManagerController.java
+++ EEIT/src/main/java/com/iii/_01_/Manager/controller/ManagerController.java
@@ -1,5 +1,11 @@
 package com.iii._01_.Manager.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,28 +13,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.iii._01_.Manager.bean.ManagerBean;
+import com.iii._01_.Manager.service.ManagerService;
 
 @Controller
 public class ManagerController {
 
-	@RequestMapping(value = "/managerLogin" , method = RequestMethod.GET)
+	@Autowired
+	ManagerService managerService;
+
+//	@ModelAttribute
+//	public ManagerBean getManagerBean() {
+//		System.out.println("ManagerBean Here");
+//		return new ManagerBean();
+//	}
+	
+	@RequestMapping(value = "/managerLogin", method = RequestMethod.GET)
 	public String getManagerLogin(Model model) {
-		
-		model.addAttribute(new ManagerBean());
-		
-		return "managerLogin";
+		model.addAttribute("ManagerBean" , new ManagerBean());
+		return "manager/managerLoginPage";
 	}
-	
-	@RequestMapping(value = "managerLogin" , method = RequestMethod.POST)
-	public String addManagerLogin(@ModelAttribute("ManagerBean")ManagerBean mb ) {
-		
-		
-		
-		
-		
-		
-		return "Backstage/Backstage";
+
+	@RequestMapping(value = "managerLogin", method = RequestMethod.POST)
+	public String addManagerLogin(@ModelAttribute("ManagerBean") ManagerBean mb, HttpSession session) {
+
+		ManagerBean managerBean = managerService.managerLoginCheck(mb.getManagerId(), mb.getManagerPwd());
+		Map<String, String> map = new HashMap<String, String>();
+		session.setAttribute("ManaError", map);
+		if (managerBean != null) {
+			session.setAttribute("ManagerLoginOK", managerBean);
+			return "redirect:backstage";
+		} else {
+			map.put("error", "帳號或密碼錯誤");
+			return "manager/managerLoginPage";
+		}
+
 	}
-	
-	
+
 }
